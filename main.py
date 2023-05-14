d1d722e90cbd901ebc5226eb3446ffa92c78933c
HoneyBreaks <919602022@qq.com>
2023-05-13 22:35:51 -0400

    Add comments to main.py

--- main.py
+++ main.py
@@ -1,38 +1,37 @@
-#IMPORT function and class from our own files
+# IMPORT function and class from our own files
 from DataLoad import dataloader
 import torch
 import argparse
 import time
 import os
-
 from transformers import  AutoModelForSequenceClassification
 from torch.optim import AdamW
 from transformers import get_linear_schedule_with_warmup
 from datasets import load_dataset
 from torch.utils.data import DataLoader
-
 import torch.cuda.amp as amp
 
 # from torch.distributed.pipeline.sync import Pipe
 # from torch.distributed import rpc
 # import tempfile
-
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-
+# Function to padding and change the tokens according to the pretrained model
 def calcuate_accuracy(preds, labels):
-  idx_max = torch.argmax(preds, dim=-1)
-  n_correct = (idx_max==labels).sum().item()
-  return n_correct
+    idx_max = torch.argmax(preds, dim=-1)
+    n_correct = (idx_max == labels).sum().item()
+    return n_correct
+
 
 def train(model, train_loader, optimizer, scheduler, rank=None, mixed=False):
     model.train()
     total_loss = 0
     num_correct = 0
     num_total = 0
-
+    # Initialize scaler
     scaler = amp.GradScaler() if mixed else None
-
+    # Start Training
     for batch in train_loader:
+        # From CPU to GPU
         if rank:
             labels = batch['labels'].cuda(rank)
             input_ids = batch['input_ids'].cuda(rank)
@@ -41,12 +40,11 @@ def train(model, train_loader, optimizer, scheduler, rank=None, mixed=False):
             labels = batch['labels'].to(device)
             input_ids = batch['input_ids'].to(device)
             attention_mask = batch['attention_mask'].to(device)
-
+        # Enable dp with single precision
         if mixed:
             with amp.autocast():
                 outputs = model(input_ids, attention_mask).logits
                 loss = torch.nn.CrossEntropyLoss()(outputs, labels)
-
             if scaler:
                 scaler.scale(loss).backward()
                 scaler.step(optimizer)
@@ -54,7 +52,7 @@ def train(model, train_loader, optimizer, scheduler, rank=None, mixed=False):
             else:
                 loss.backward()
                 optimizer.step()
-
+            # Update parameters
             scheduler.step()  
             optimizer.zero_grad()
         else:
@@ -64,15 +62,14 @@ def train(model, train_loader, optimizer, scheduler, rank=None, mixed=False):
             optimizer.step()
             scheduler.step() 
             optimizer.zero_grad()
-
+        # Calculate loss and acc
         num_correct += calcuate_accuracy(outputs, labels)
         total_loss += loss.item()
         num_total += labels.size(0)
-
     avg_train_loss = total_loss / num_total
     avg_train_acc = num_correct / num_total * 100.0
-
     return avg_train_loss, avg_train_acc
+
 
 def evaluate(model, test_loader, rank=None):
     model.eval()
@@ -80,8 +77,10 @@ def evaluate(model, test_loader, rank=None):
     total_correct = 0
     total_samples = 0
     num_total = 0
+    # Start evaluation
     with torch.no_grad():
         for batch in test_loader:
+            # From CPU to GPU
             if rank:
                 labels = batch['labels'].cuda(rank)
                 input_ids = batch['input_ids'].cuda(rank)
@@ -90,55 +89,49 @@ def evaluate(model, test_loader, rank=None):
                 labels = batch['labels'].to(device)
                 input_ids = batch['input_ids'].to(device)
                 attention_mask = batch['attention_mask'].to(device)
-                
+            # Get output and calculate loss
             output = model(input_ids, attention_mask).logits
             loss = torch.nn.CrossEntropyLoss()(output, labels)
-
+            # Calculate loss and acc
             total_loss += loss.item()
             total_correct += calcuate_accuracy(output, labels)
             total_samples += labels.size(0)
             num_total += labels.size(0)
-
     average_loss = total_loss / num_total
     accuracy = (total_correct / num_total) * 100.0
 
     return average_loss, accuracy
 
+
 def main(rank=None, world_size=None, opts=None):
-       
+    # Get dataloader and model
     train_loader, test_loader, train_datasets, test_datasets = dataloader(name="ag_news", token_name=opts.pretrained_model_name, train_length=10000, batch_size=opts.batch_size)
     model = AutoModelForSequenceClassification.from_pretrained(opts.pretrained_model_name, num_labels=opts.num_classes)
-    
+    # Enable DP
     if opts.DP:
         device_ids = [i for i in range(world_size)]
         model = torch.nn.DataParallel(model, device_ids=device_ids)
-        
+    # Send model to GPU
     model.to(device)
-    
-    # Set the optimizer
+    # Set the optimizer and learning rate
     optimizer = AdamW(model.parameters(), lr=5e-5, weight_decay=0.0001)
-
     num_epochs = opts.epoch
     num_training_steps = num_epochs * len(train_loader)
     num_warmup_steps = int(0.1 * num_training_steps)  # Adjust the warmup steps as needed
-
     scheduler = get_linear_schedule_with_warmup(
         optimizer,
         num_warmup_steps=num_warmup_steps,
         num_training_steps=num_training_steps
     )
-    
+    # Training for epochs
     for epoch in range(opts.epoch):
         start_time = time.time()
-        
         avg_train_loss, avg_train_acc = train(model, train_loader, optimizer, scheduler, rank=rank, mixed=opts.mixed)
-        
         end_time = time.time()
-        
         avg_test_loss, avg_test_acc = evaluate(model, test_loader, rank)
-        
+        # calculate time
         epoch_time = end_time - start_time
-        
+        # Print results
         print("Epoch: ", (epoch+1))
         print(f'\tTrain Loss: {avg_train_loss:.5f} | Train Acc: {avg_train_acc:.2f}%')
         print(f'\tTest. Loss: {avg_test_loss:.5f} |  Test Acc: {avg_test_acc:.2f}%')
@@ -146,10 +139,9 @@ def main(rank=None, world_size=None, opts=None):
     
 
 if __name__ == "__main__":
-    
+    # Get parser argument
     num_gpu = torch.cuda.device_count()
     print(f"You are using {num_gpu} GPUS!")
-    
     parser = argparse.ArgumentParser()
     parser.add_argument('--pretrained_model_name', type=str, default='bert-base-uncased', help='Name of the pre-trained BERT model')
     parser.add_argument('--epoch', type=int, default=2, help='Number of training epoches')
@@ -159,14 +151,12 @@ if __name__ == "__main__":
     parser.add_argument('--mixed', action="store_true", help='enable mixed-precision training')
     parser.add_argument('--batch_size', type=int, default=8, help='Batch size for training and testing')
     opts = parser.parse_args()
-    
     # enable Distributed Data parallel
     if opts.DP:
-        # device_ids = [i for i in range(num_gpu)]
-        # mp.spawn(main, args=(num_gpu, opts), nprocs=num_gpu, join=True)
         main(world_size=num_gpu, opts=opts)
     else:
         main(world_size=1, opts=opts)
+
        
         
         
